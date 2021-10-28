c5e4e6ab47cce48d7b0527b09a3bd3f18961c829
derek-corcoran-barrios <derek.corcoran.barrios@gmail.com>
2021-10-28 07:06:11 +0200

    Test updated

--- TestGitScript.R
+++ TestGitScript.R
@@ -1,1 +1,6 @@
 library(usethis)
+usethis::use_git()
+#usethis::create_github_token() if you dont have it
+# usethis::use_git_config()
+gitcreds::gitcreds_set()
+usethis::use_github(protocol = "https")
